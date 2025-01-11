92d94dd423eccff3e82cf8fdb0ae5c61947647f3
Bánfalvi-Kovács Tivadar <bktb888@icloud.com>
2025-01-11 19:15:21 +0100

    small improvements

--- Mankala.cpp
+++ Mankala.cpp
@@ -34,18 +34,9 @@ void Mankala::makeStats(const unsigned int numOfPlays) {
     std::vector<Result> results(numOfPlays);
     for (unsigned i=0;i<numOfPlays;++i)
         results[i]=this->play();
-    unsigned p1_wins = std::count(results.begin(), results.end(), Result::player1),
-             p2_wins = std::count(results.begin(), results.end(), Result::player2);
+    const unsigned p1_wins = std::count(results.begin(), results.end(), Result::player1);
+    const unsigned p2_wins = std::count(results.begin(), results.end(), Result::player2);
 
-    std::cout<<"Player1 percentage: " << (double) p1_wins/numOfPlays << std::endl
-             <<"Player2 percentage: " << (double) p2_wins/numOfPlays << std::endl;
-}
-
-void tester(Mankala& mankala, unsigned numOfGames){
-    auto start = std::chrono::high_resolution_clock::now();
-    mankala.makeStats(numOfGames);
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double, std::micro> duration = end - start;
-    std::cout << "A single game on average: " << duration.count()/numOfGames << " microseconds" << std::endl;
+    std::cout<<"Player1 percentage: " << static_cast<double>(p1_wins)/numOfPlays << std::endl
+             <<"Player2 percentage: " << static_cast<double>(p2_wins)/numOfPlays << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include "Mankala.h"
+#include <chrono>
 
+void tester(Mankala& mankala, const unsigned numOfGames){
+    const auto start = std::chrono::high_resolution_clock::now();
+    mankala.makeStats(numOfGames);
+    const auto end = std::chrono::high_resolution_clock::now();
 
+    const std::chrono::duration<double, std::micro> duration = end - start;
+    std::cout << "A single game on average: " << duration.count()/numOfGames << " microseconds" << std::endl;
+}
+const AI maxy = create_AI([] [[gnu::const]](const BoardState& board) -> float {
+
+    const auto opSide=board.getOpponentSide();
+    const unsigned char ballsInPlay =72 - board.getMyTrash() - board.getOpponentsTrash();
+    const unsigned char opBalls = std::accumulate(opSide.begin(), opSide.end(), 0);
+
+    return static_cast<float>(opBalls)/ballsInPlay;
+});
 int main() {
     Mankala test(randy, randy);
 
-    int numOfGames=100000;
+    constexpr int numOfGames=1000000;
 
     Mankala test2(randy2, randy2);
-    Mankala test3(randy2, minimax);
+    Mankala test3(randy2, maxy);
 
-    tester(test, numOfGames);
     tester(test2, numOfGames);
     tester(test3, numOfGames);
 
